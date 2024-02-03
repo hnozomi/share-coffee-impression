00b0fb33f213f556e525c1bb69b5a78ff879e381
濱片希望 <47496184+hnozomi@users.noreply.github.com>
2024-02-03 17:37:39 +0900

    ルーティング部分のリファクタリング

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,56 +12,56 @@ use Illuminate\Validation\ValidationException;
 class AuthController extends Controller
 {
 
-public function register(Request $request)
-{
+    public function register(Request $request)
+    {
 
-    Log::warning($request);
+        Log::warning($request);
 
-$request->validate([
-'name' => 'required|string|max:255',
-'age' => 'required|integer',
-'email' => 'required|string|email|max:255|unique:users',
-'password' => 'required|string|min:6',
-]);
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'age' => 'required|integer',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
 
-$user = User::create([
-'name' => $request->name,
-'age' => $request->age,
-'email' => $request->email,
-'password' => Hash::make($request->password),
-]);
+        $user = User::create([
+            'name' => $request->name,
+            'age' => $request->age,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
 
-return response()->json(['message' => 'User successfully registered', 'user' => $user]);
-}
-
-public function login(Request $request)
-{
-    $credentials = $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-    ]);
-
-    Log::debug(print_r($credentials, true));
-
-    if (!Auth::attempt($credentials)) {
-        return response()->json(['message' => 'Invalid credentials'], 401);
+        return response()->json(['message' => 'User successfully registered', 'user' => $user]);
     }
 
-    $request->session()->regenerate();
-    return response()->json(Auth::user());
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
 
-}
+        Log::debug(print_r($credentials, true));
 
-public function logout(Request $request)
-{
-    // セッションを無効化してユーザーをログアウトする
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $request->session()->regenerate();
+        return response()->json(Auth::user());
+
+    }
+
+    public function logout(Request $request)
+    {
+        // セッションを無効化してユーザーをログアウトする
 //    Auth::logout();
-    $request->session()->invalidate();
+        $request->session()->invalidate();
 
-    // 再生成して古いセッションとトークンを無効にするために再生成
-    $request->session()->invalidate();
-    $request->session()->regenerateToken();
+        // 再生成して古いセッションとトークンを無効にするために再生成
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
-    return response()->json(['message' => 'User successfully logged out']);
-}
+        return response()->json(['message' => 'User successfully logged out']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -41,19 +41,24 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 //Route::post('/auth/signup', [UserController::class, 'signup'])->name('signup');
 //Route::post('/auth/login', [UserController::class, 'login'])->name('login');
 
-// コーヒーの感想一覧を取得
-Route::middleware('auth:sanctum')->get('/coffee_impression', [CoffeeImpressionController::class, 'fetchAllCoffeeImpression'])->name('fetchAllCoffeeImpression');
-// 自分が投稿したコーヒーの感想を取得
-Route::middleware('auth:sanctum')->get('/coffee_impression/my_impression', [CoffeeImpressionController::class, 'fetchMyCoffeeImpression'])->name('fetchMyCoffeeImpression');
-// コーヒーの感想一覧を追加
-Route::post('/coffee_impression/add', [CoffeeImpressionController::class, 'addCoffeeImpressions'])->name('addCoffeeImpressions');
-// 選択されたコーヒーの感想を取得
-Route::get('/coffee_impression/{coffee_impression_id}', [CoffeeImpressionController::class, 'fetchOnlyCoffeeImpression'])->name('fetchOnlyCoffeeImpression');
-// コーヒーの感想を更新
-Route::put('/coffee_impression/update/{coffee_impression_id}', [CoffeeImpressionController::class, 'updateCoffeeImpression'])->name('updateCoffeeImpression');
-// コーヒーの感想を削除
-Route::delete('/coffee_impression/delete/{coffee_impression_id}', [CoffeeImpressionController::class, 'deleteCoffeeImpression'])->name('deleteCoffeeImpression');
+Route::middleware('auth:sanctum')->group(function () {
 
+    Route::prefix('coffee_impression')->group(function () {
+        // コーヒーの感想一覧を取得
+        Route::get('/', [CoffeeImpressionController::class, 'fetchAllCoffeeImpression'])->name('fetchAllCoffeeImpression');
+        // 自分が投稿したコーヒーの感想を取得
+        Route::get('/my_impression', [CoffeeImpressionController::class, 'fetchMyCoffeeImpression'])->name('fetchMyCoffeeImpression');
+        // コーヒーの感想一覧を追加
+        Route::post('/add', [CoffeeImpressionController::class, 'addCoffeeImpressions'])->name('addCoffeeImpressions');
+        // 選択されたコーヒーの感想を取得
+        Route::get('/{coffee_impression_id}', [CoffeeImpressionController::class, 'fetchOnlyCoffeeImpression'])->name('fetchOnlyCoffeeImpression');
+        // コーヒーの感想を更新
+        Route::put('/update/{coffee_impression_id}', [CoffeeImpressionController::class, 'updateCoffeeImpression'])->name('updateCoffeeImpression');
+        // コーヒーの感想を削除
+        Route::delete('/delete/{coffee_impression_id}', [CoffeeImpressionController::class, 'deleteCoffeeImpression'])->name('deleteCoffeeImpression');
+    });
 
-// コメントを追加
-Route::post('/comment/add', [CommentController::class, 'addComment'])->name('addComment');
+    // コメントを追加
+    Route::post('/comment/add', [CommentController::class, 'addComment'])->name('addComment');
+});
+
